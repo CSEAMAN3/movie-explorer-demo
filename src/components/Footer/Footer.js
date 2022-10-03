58c9e181e0daec92f75bacbdb618fd9d9cbd3e36
Chris Seaman <christopherseaman1983@gmail.com>
2022-10-03 18:56:10 +0100

    initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <h1>This is a footer</h1>
+      </div>
+    </footer>
+  );
+}
